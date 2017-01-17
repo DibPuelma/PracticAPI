d8593d19f5e811d1016e1b351ccb8820264a309d
dibpuelma <edib@uc.cl>
2017-01-17 19:45:07 -0300

    Changed some creates to builds. Errors still poping

--- seeders/customPollSeeder.js
+++ seeders/customPollSeeder.js
@@ -17,112 +17,90 @@ var users = [];
 
 //Creamos usuarios
 for (var i = 0; i < 4; i++) {
-  Inde.User.create({
+  var user = Inde.User.build({
     email: i + "@gmail.com",
     firstName: "Roberto " + i,
     lastName: "Perez " + i,
     birthdate: Date.now(),
     gender: 'm',
     password: "123123123" + i
-  }).then(function(user){
-    users.push(user);
-    console.log("pusheado");
-  });
+  })
+  users.push(user);
+  console.log("pusheado");
 }
+
 var OC1, OC2, OC3, Q1, Q2, Q3, Q4, Q5;
 
 //Creamos contenedores de opciones
-Inde.OptionsContainer.create({
+var OC1 = Inde.OptionsContainer.build({
   name: "Comidas",
   allow_other: true
-}).then(function(optcont) {
-  OC1 = optcont;
-});
+})
 
-Inde.OptionsContainer.create({
+var OC2 = Inde.OptionsContainer.build({
   name: "Colores",
   allow_other: false
-}).then(function(optcont) {
-  OC2 = optcont;
-});
+})
 
-Inde.OptionsContainer.create({
+var OC3 = Inde.OptionsContainer.build({
   name: "Géneros Musicales",
   allow_other: true
-}).then(function(optcont) {
-  OC3 = optcont;
-});
+})
 
 //Creamos las preguntas
-Inde.Question.create({
+var Q1 = Inde.Question.build({
   text: "¿Cúal es tu comida favorita?",
   type: "options"
-}).then(function(question) {
-  Q1 = question;
-  question.setOptionsContainer(OC1);
 })
+Q1.setOptionsContainer(OC1);
 
-Inde.Question.create({
+var Q2 = Inde.Question.build({
   text: "¿Cúal es tu color favorito?",
   type: "options"
-}).then(function(question) {
-  Q2 = question;
-  question.setOptionsContainer(OC2);
 })
+Q2.setOptionsContainer(OC2);
 
-Inde.Question.create({
+
+var Q3 = Inde.Question.build({
   text: "¿Cúal es tu género musical favorito?",
   type: "options"
-}).then(function(question) {
-  Q3 = question;
-  question.setOptionsContainer(OC3);
 })
+Q3.setOptionsContainer(OC3);
 
-Inde.Question.create({
+var Q4 = Inde.Question.build({
   text: "Si quieres nos puedes dejar un comentario",
   type: "text"
-}).then(function(question) {
-  Q4 = question;
 })
 
-Inde.Question.create({
+var Q5 = Inde.Question.build({
   text: "¿Qué nota le pondrías al local?",
   type: "number"
-}).then(function(question) {
-  Q5 = question;
 })
 
 
 //Creamos opciones
 for (var option in optionsArray1) {
-  Inde.PosibleOption.create({
+  var posibleoption = Inde.PosibleOption.build({
     value: option
-  }).then(function(posibleoption){
-    OC1.addPosibleOption(posibleoption);
-    optionsObjectFood.push(posibleoption);
   });
+  OC1.addPosibleOption(posibleoption);
+  optionsObjectFood.push(posibleoption);
 }
 
 for (var option in optionsArray2) {
-  Inde.PosibleOption.create({
+  var posibleoption = Inde.PosibleOption.build({
     value: option
-  }).then(function(posibleoption){
-    OC2.addPosibleOption(posibleoption);
-    optionsObjectColor.push(posibleoption);
   });
+  OC2.addPosibleOption(posibleoption);
+  optionsObjectColor.push(posibleoption);
 }
 
 for (var option in optionsArray3) {
-  Inde.PosibleOption.create({
+  var posibleoption = Inde.PosibleOption.build({
     value: option
-  }).then(function(posibleoption){
-    OC3.addPosibleOption(posibleoption);
-    optionsObjectMusic.push(posibleoption);
   });
-}
-
-while (optionsObjectFood.length != 7 || optionsObjectColor.length != 5 || optionsObjectMusic.length != 5){
-
+  OC3.addPosibleOption(posibleoption);
+  optionsObjectMusic.push(posibleoption);
 }
 
 //Creamos las encuestas
@@ -200,5 +178,5 @@ for (var i = 0; i < 5; i++) {
 }
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
