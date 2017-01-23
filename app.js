115778c76bc31f55978dc01a6897698a396ef477
dibpuelma <edib@uc.cl>
2017-01-23 13:34:58 -0300

    Beautify app.js

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express          = require('express');
+var app              = express();
+var bodyParser       = require('body-parser');
 var expressValidator = require('express-validator');
 
 // app.use(require('./controllers'));
 app.use(bodyParser.json());
 app.use(expressValidator([]));
 
-var Poll = require('./controllers/poll.js');
-var Question = require('./controllers/question.js');
+var Poll             = require('./controllers/poll.js');
+var Question         = require('./controllers/question.js');
 var OptionsContainer = require('./controllers/optionscontainer.js');
-var PossibleOption = require('./controllers/possibleoption.js');
-var AnsweredPoll = require('./controllers/answeredpoll.js');
-var Answer = require('./controllers/answer.js');
+var PossibleOption   = require('./controllers/possibleoption.js');
+var AnsweredPoll     = require('./controllers/answeredpoll.js');
+var Answer           = require('./controllers/answer.js');
 
 
 
 //Encuestas
-app.get('company/:company_id/poll', Poll.index)
-app.post('company/:company_id/poll', Poll.create);
-app.get('company/:company_id/poll/:id', Poll.show);
-app.delete('company/:company_id/poll/:id', Poll.delete);
-app.put('company/:company_id/poll/:id', Poll.update);
+app.get('company/:company_id/poll',                                   Poll.index)
+app.post('company/:company_id/poll',                                  Poll.create);
+app.get('company/:company_id/poll/:id',                               Poll.show);
+app.delete('company/:company_id/poll/:id',                            Poll.delete);
+app.put('company/:company_id/poll/:id',                               Poll.update);
 app.put('company/:company_id/poll/:poll_id/sellpoint/:sell_point_id', Poll.changeActiveSellPoint);
 
 //Preguntas
-app.post('company/:company_id/question', Question.create);
-app.get('company/:company_id/question/:id', Question.show);
-app.delete('company/:company_id/question/:id', Question.delete);
-app.put('company/:company_id/question/:id', Question.update);
+app.post('company/:company_id/question',                     Question.create);
+app.get('company/:company_id/question/:id',                  Question.show);
+app.delete('company/:company_id/question/:id',               Question.delete);
+app.put('company/:company_id/question/:id',                  Question.update);
 app.delete('company/:company_id/poll/:poll_id/question/:id', Question.removeQuestionFromPoll);
-app.put('company/:company_id/poll/:poll_id/question/:id', Question.addQuestionToPoll);
+app.put('company/:company_id/poll/:poll_id/question/:id',    Question.addQuestionToPoll);
 
 //Contenedores de opciones
-app.get('company/:company_id/optcont', OptionsContainer.index);
-app.post('company/:company_id/optcont', OptionsContainer.create);
-app.get('company/:company_id/optcont/:id', OptionsContainer.show);
+app.get('company/:company_id/optcont',        OptionsContainer.index);
+app.post('company/:company_id/optcont',       OptionsContainer.create);
+app.get('company/:company_id/optcont/:id',    OptionsContainer.show);
 app.delete('company/:company_id/optcont/:id', OptionsContainer.delete);
-app.put('company/:company_id/optcont/:id', OptionsContainer.update);
+app.put('company/:company_id/optcont/:id',    OptionsContainer.update);
 
 //Opciones
-app.get('company/:company_id/possopt', PossibleOption.index);
-app.post('company/:company_id/possopt', PossibleOption.create);
-app.get('company/:company_id/possopt/:id', PossibleOption.show);
-app.delete('company/:company_id/possopt/:id', PossibleOption.delete);
-app.put('company/:company_id/possopt/:id', PossibleOption.update);
+app.get('company/:company_id/possopt',                             PossibleOption.index);
+app.post('company/:company_id/possopt',                            PossibleOption.create);
+app.get('company/:company_id/possopt/:id',                         PossibleOption.show);
+app.delete('company/:company_id/possopt/:id',                      PossibleOption.delete);
+app.put('company/:company_id/possopt/:id',                         PossibleOption.update);
 app.delete('company/:company_id/optcont/:opt_cont_id/possopt/:id', PossibleOption.removeOptionFromContainer);
-app.put('company/:company_id/optcont/:opt_cont_id/possopt/:id', PossibleOption.addOptionToContainer);
+app.put('company/:company_id/optcont/:opt_cont_id/possopt/:id',    PossibleOption.addOptionToContainer);
 
 //Encuestas Contestadas
-app.post('company/:company_id/poll/:poll_id/answered_poll', AnsweredPoll.create);
-app.get('user/:user_id/answered_poll/:id', AnsweredPoll.indexByUser);
-app.get('company/:company_id/poll/:poll_id/answered_poll/:id', AnsweredPoll.show);
+app.post('company/:company_id/poll/:poll_id/answered_poll',       AnsweredPoll.create);
+app.get('user/:user_id/answered_poll/:id',                        AnsweredPoll.indexByUser);
+app.get('company/:company_id/poll/:poll_id/answered_poll/:id',    AnsweredPoll.show);
 app.delete('company/:company_id/poll/:poll_id/answered_poll/:id', AnsweredPoll.delete);
 
 //Preguntas
